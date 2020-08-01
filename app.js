ba4327b764971c68eb01910e653ebcdd4c4c4f2e
maneesht88 <maneesh.marian@gmail.com>
2020-08-01 10:17:34 +0530

    4th

--- app.js
+++ app.js
@@ -6,18 +6,85 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/registerdb", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/marianregidb", {useNewUrlParser: true, useUnifiedTopology: true});
 
 //mongodb+srv://maneesht88:<password>@cluster0-wxzpv.mongodb.net/test?retryWrites=true&w=majority
 // mongo "mongodb+srv://cluster0-wxzpv.mongodb.net/test"  --username maneesht88
-// mongoose.connect("mongodb+srv://maneesht88:maneesh@123@cluster0-wxzpv.mongodb.net/todolistdb", {useNewUrlParser: true, useUnifiedTopology: true});
-const itemsSchema={name:String};
-const Item=mongoose.model("item",itemsSchema);
+mongoose.connect("mongodb+srv://maneesht88:maneesh@123@cluster0-wxzpv.mongodb.net/marianregidb", {useNewUrlParser: true, useUnifiedTopology: true});
+const articleSchema = {
+  fname:String,
+  school: String,
+  phone:String,
+  mail:String,
+  board:String,
+  phy:String,
+  chem:String,
+  maths:String,
+  bomech:String,
+  bocs:String,
+  boee:String,
+  boec:String,
+  bocvil:String }
+const Article = mongoose.model("Article", articleSchema);
 app.get("/", function(req, res) {
 res.render("index.ejs")
 });
 app.post("/", function(req, res) {
-res.render("thankyou.ejs")
+  console.log(req.body.fname);
+  phone=req.body.phone;
+  const newArticle = new Article({
+  fname: req.body.fname,
+  school: req.body.school,
+phone:req.body.phone,
+  mail:req.body.mail,
+  board:req.body.board,
+  phy:req.body.phy,
+  chem:req.body.chem,
+  maths:req.body.maths,
+  bomech:req.body.bomech,
+  bocs:req.body.bocs,
+  boee:req.body.boee,
+  boec:req.body.boec,
+  bocvil:req.body.bocvil,
+})
+newArticle.save(function(err) {
+  if (!err) {
+    Article.findOne({phone:phone},function(err,foundList){
+      if(!err){
+  res.render("thankyou.ejs", {
+    fname: foundList.fname,
+    school: foundList.school,
+    phone:foundList.phone,
+    mail:foundList.mail,
+    board:foundList.board,
+    phy:foundList.phy,
+    chem:foundList.chem,
+    maths:foundList.maths,
+    bomech:foundList.bomech,
+    bocs:foundList.bocs,
+    boee:foundList.boee,
+    boec:foundList.boec,
+    bocvil:foundList.bocvil,
+  }
+  );
+  res.render("thankyou.ejs");
+  }
+  else{
+    res.send("error");
+  }
+  });
+
+  } else {
+    res.send("tryagain");
+  }
+});
+
+});
+
+
+app.get("/thankyou", function(req, res) {
+
+
 });
 
 
